9378e22ec5145ae31ab139fd87ed83756b623d05
David Nicklay <david.nicklay@turner.com>
2014-07-03 15:35:06 -0400

    spelling fix

--- config/software/logstash-forwarder.rb
+++ config/software/logstash-forwarder.rb
@@ -3,7 +3,7 @@ name "logstash-forwarder"
 default_version "v0.3.1"
 
 source :git => "https://github.com/elasticsearch/logstash-forwarder.git"
-relative_path "logstash-fowarder"
+relative_path "logstash-forwarder"
 
 build do
   command "make", :env => configure_env
